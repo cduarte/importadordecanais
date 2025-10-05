2981024b98cfccaa888bd3c92fde865ae6a962e6
win_home <win_home>
2025-10-05 21:07:27 +0100

    ajuste

--- server/worker_process_canais.php
+++ server/worker_process_canais.php
@@ -218,7 +218,7 @@ function processJob(PDO $adminPdo, array $job, int $streamTimeout): array
     updateJob($adminPdo, $jobId, [
         'm3u_file_path' => $fullPath,
         'progress' => CHANNEL_PROGRESS_DOWNLOAD,
-        'message' => 'Lista M3U verfificada. Conectando ao banco de destino...'
+        'message' => 'Lista M3U verificada. Conectando ao banco de destino...'
     ]);
 
     try {
